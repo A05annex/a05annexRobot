ec0a44f6b3569ce3801e0f7e45aa68ef06288b45
STHobbes <royster.hall@gmail.com>
2024-03-22 18:33:43 -0700

    This ism a test branch where I am trying to hook the pi to my iMac or MacBook Pro so I can run our Photonvision code in a debugging environment.

--- src/test/java/org/a05annex/frc/subsystems/MonitorPhotonCameraWrapper.java
+++ src/test/java/org/a05annex/frc/subsystems/MonitorPhotonCameraWrapper.java
@@ -11,8 +11,13 @@ import java.io.IOException;
 import edu.wpi.first.cscore.CameraServerJNI;
 import edu.wpi.first.math.WPIMathJNI;
 import edu.wpi.first.util.WPIUtilJNI;
+import org.a05annex.util.AngleD;
+import org.a05annex.util.AngleUnit;
+import org.photonvision.PhotonCamera;
 
 public class MonitorPhotonCameraWrapper {
+
+
     public static void main(String[] args) throws IOException {
         NetworkTablesJNI.Helper.setExtractOnStaticLoad(false);
         WPIUtilJNI.Helper.setExtractOnStaticLoad(false);
@@ -20,7 +25,7 @@ public class MonitorPhotonCameraWrapper {
         CameraServerJNI.Helper.setExtractOnStaticLoad(false);
 
         CombinedRuntimeLoader.loadLibraries(MonitorPhotonCameraWrapper.class, "wpiutiljni", "wpimathjni", "ntcorejni",
-                "cscorejnicvstatic");
+                "cscorejni");
         new MonitorPhotonCameraWrapper().run();
     }
 
@@ -31,7 +36,13 @@ public class MonitorPhotonCameraWrapper {
         DoubleSubscriber ySub = table.getDoubleTopic("y").subscribe(0.0);
         inst.startClient4("example client");
         inst.setServer("10.68.31.12"); // where TEAM=190, 294, etc, or use inst.setServer("hostname") or similar
-        //       inst.startDSClient(); // recommended if running on DS computer; this gets the robot IP from the DS
+        inst.startDSClient(); // recommended if running on DS computer; this gets the robot IP from the DS
+
+
+        PhotonCameraWrapper CAMERA =
+                new PhotonCameraWrapper(new PhotonCamera("Arducam_OV9281_USB_Camera"),
+                        0.32, new AngleD(AngleUnit.DEGREES, 24.5));
+
         while (true) {
             try {
                 Thread.sleep(1000);
